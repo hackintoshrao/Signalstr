8c91c50f09467fd43e1a461be9e8d87c3d53a781
Karthic Rao <kartronics85@gmail.com>
2013-01-16 08:09:30 +0530

    first commit

---
+++ ui_about.h
@@ -0,0 +1,50 @@
+/********************************************************************************
+** Form generated from reading UI file 'about.ui'
+**
+** Created: Wed Jan 16 07:56:09 2013
+**      by: Qt User Interface Compiler version 4.8.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_ABOUT_H
+#define UI_ABOUT_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QDialog>
+#include <QtGui/QHeaderView>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_About
+{
+public:
+
+    void setupUi(QDialog *About)
+    {
+        if (About->objectName().isEmpty())
+            About->setObjectName(QString::fromUtf8("About"));
+        About->resize(585, 211);
+
+        retranslateUi(About);
+
+        QMetaObject::connectSlotsByName(About);
+    } // setupUi
+
+    void retranslateUi(QDialog *About)
+    {
+        About->setWindowTitle(QApplication::translate("About", "Dialog", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class About: public Ui_About {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_ABOUT_H
---
+++ ui_widget.h
@@ -0,0 +1,122 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created: Mon Oct 29 05:24:44 2012
+**      by: Qt User Interface Compiler version 4.8.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QLineEdit>
+#include <QtGui/QPushButton>
+#include <QtGui/QSpacerItem>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QHBoxLayout *horizontalLayout_2;
+    QPushButton *okButton;
+    QPushButton *cancelButton;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *pushButton;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QString::fromUtf8("Widget"));
+        Widget->resize(458, 192);
+        Widget->setWindowOpacity(0.85);
+        verticalLayout = new QVBoxLayout(Widget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        label = new QLabel(Widget);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        horizontalLayout->addWidget(label);
+
+        lineEdit = new QLineEdit(Widget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        horizontalLayout->addWidget(lineEdit);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        okButton = new QPushButton(Widget);
+        okButton->setObjectName(QString::fromUtf8("okButton"));
+        okButton->setEnabled(false);
+        okButton->setDefault(true);
+
+        horizontalLayout_2->addWidget(okButton);
+
+        cancelButton = new QPushButton(Widget);
+        cancelButton->setObjectName(QString::fromUtf8("cancelButton"));
+
+        horizontalLayout_2->addWidget(cancelButton);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_2->addItem(horizontalSpacer);
+
+        pushButton = new QPushButton(Widget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        horizontalLayout_2->addWidget(pushButton);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+#ifndef QT_NO_SHORTCUT
+        label->setBuddy(lineEdit);
+#endif // QT_NO_SHORTCUT
+        QWidget::setTabOrder(okButton, lineEdit);
+        QWidget::setTabOrder(lineEdit, cancelButton);
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QApplication::translate("Widget", "RAO-SIGNALDESCP", 0, QApplication::UnicodeUTF8));
+        label->setText(QApplication::translate("Widget", "&SIGNAL.NO:", 0, QApplication::UnicodeUTF8));
+        okButton->setText(QApplication::translate("Widget", "&OK", 0, QApplication::UnicodeUTF8));
+        cancelButton->setText(QApplication::translate("Widget", "&Cancel", 0, QApplication::UnicodeUTF8));
+        pushButton->setText(QApplication::translate("Widget", "&About..", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
